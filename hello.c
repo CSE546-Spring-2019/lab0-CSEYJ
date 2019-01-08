da01ff50b7df78a6d8186adc7f5216bfba02c99b
CSEYJ <42721277+CSEYJ@users.noreply.github.com>
2019-01-07 20:58:37 -0500

    Add comments on hello.c

--- hello.c
+++ hello.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 int main()
 {
+  /*printf statement to output a string of Hello world, I am Yongjeong Kim.*/
   printf ("Hello world, I am Yongjeong Kim.\n");
   return 0;
-  }
+}
